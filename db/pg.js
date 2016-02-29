e9a4f4dc1a4b6298fbf5917958405f62a897711d
Emma Hou <emma.houy@gmail.com>
2016-02-29 15:08:14 -0500

    heroku

--- db/pg.js
+++ db/pg.js
@@ -1,5 +1,10 @@
 var pg = require('pg');
-var connectionString = "postgres://emmahou:900118@localhost/petmeetup";
+
+if(process.env.ENVIRONMENT === 'Production'){
+  var config - process.env.DATABASE_URL
+}else{
+  var connectionString = "postgres://emmahou:900118@localhost/petmeetup";
+}
 var bcrypt = require('bcrypt');
 var salt = bcrypt.genSaltSync(10);
 var session = require('express-session');
